c5a9ad2d54a4b2d42d970766aa75e5629973b46d
Edward Kaboggoza <eddiegoza@gmail.com>
2024-03-08 19:55:17 +0300

    initial commit

---
+++ agri1.py
@@ -0,0 +1,9 @@
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import mean_squared_error, r2_score
+import mlflow
+import mlflow.sklearn
+import joblib
